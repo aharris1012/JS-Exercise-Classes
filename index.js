8aac5aad5ddd3c5802d890477f596301c83411c3
Andrea Harris <Andrea@zacs-imac.lan>
2019-11-14 18:57:02 -0600

    test 2

--- index.js
+++ index.js
@@ -41,6 +41,22 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood){
+    if (this.stomach.length <= 9){
+      this.stomach.push(someFood);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 
 }
 
@@ -61,7 +77,7 @@ class Person {
 class Car {
 
 }
-class new car 
+
 /*
   TASK 3
     - Write a Lambdasian class.
